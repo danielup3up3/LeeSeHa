671943d940ed45deccd469de12691564e673b8d4
danielup3up3 <37258273+danielup3up3@users.noreply.github.com>
2019-10-01 23:05:49 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -86,7 +86,7 @@ function parseInput(rplyToken, inputStr) {
 	if (trigger.match(/立flag|死亡flag/) != null) return exports.funny.BStyleFlagSCRIPTS() ;	
 	
 	//鴨霸獸指令開始於此
-	if (trigger.match(/Bang|bang/) != null) return exports.funny.randomReply() ;
+	if (trigger.match(/傑斯|傑斯貝連/) != null) return exports.funny.randomReply() ;
 	if (trigger.match(/C8763|相位終結技/) != null) return exports.funny.C8763() ;
 	if (trigger.match(/^野$/) != null) return exports.funny.ya() ;
 	if (trigger.match(/^炸$/) != null) return exports.funny.zA() ;
